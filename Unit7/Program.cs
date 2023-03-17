1c62f281d35ef7e6963a574a036561cd06de14fc
Максим Самисько <masechk@gmail.com>
2023-03-17 15:48:08 +0300

    Unit7: Added constructors for Person, User, Manager

--- Unit7/Program.cs
+++ Unit7/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,8 +14,8 @@ namespace Unit7
     abstract class Person
     {
         private string login;
-        public string Login 
-        { 
+        public string Login
+        {
             get { return login; }
             set
             {
@@ -22,13 +23,13 @@ namespace Unit7
                     login = value;
                 else
                     Console.WriteLine("Ooops! Login can't be empty");
-            }   
+            }
         }
         private string password;
         public string Password
         {
-            get { return password; } 
-            set 
+            get { return password; }
+            set
             {
                 if (value != null)
                     password = value;
@@ -36,14 +37,56 @@ namespace Unit7
                     Console.WriteLine("Ooops! Password can't be empty");
             }
         }
+        public Person(string log, string pas)
+        {
+            password = pas;
+            login = log;
+        }
     }
     class User : Person
     {
-
+        private double balance;
+        public double Balance
+        {
+            get { return balance; }
+            set
+            {
+                if (value >= 0)
+                    balance = value;
+                else
+                    Console.WriteLine("Ooops! You have not enough money to by");
+            }
+        }
+        public User(double mon) : base(Console.ReadLine(), Console.ReadLine())
+        {
+            if (mon >= 0)
+                balance = mon;
+            else
+                Console.WriteLine("Ooops! User can't have negative balance");
+        }
     }
     class Manager : Person
     {
+        private int id;
+        public int Id
+        {
+            get { return id; }
+            set
+            {
+                if (value > 1000)
+                    id = value;
+                else
+                    Console.WriteLine("Ooops! Manager ID mast be bigger than 1000");
 
+            }
+        }
+        public Manager(int id) : base(Console.ReadLine(), Console.ReadLine())
+        {
+            if (id > 1000)
+                this.id = id;
+            else
+                Console.WriteLine("Ooops! Manager ID mast be bigger than 1000");
+        }
     }
 
     abstract class Delivery
@@ -65,7 +108,7 @@ namespace Unit7
     {
         /* ... */
     }
-    class Product <TArticle>
+    class Product
     {
         private string name;
         public string Name
@@ -80,14 +123,14 @@ namespace Unit7
             }
         }
         // Article of product can't bee empty
-        private TArticle articlee;
-        public TArticle Articlee
+        private string article;
+        public string Article
         {
-            get { return articlee; }
+            get { return article; }
             set
             {
                 if (value != null)
-                    articlee = value;
+                    article = value;
                 else
                     Console.WriteLine("Ooops! Article can't bee empty");
             }
@@ -106,21 +149,24 @@ namespace Unit7
             }
         }
         public string Description;
+        // Count of products in the shop
         private int count;
         public int Count
         {
             get { return count; }
             set
             {
-                if (value <= count)
-                    count -= value;
+                if (value >= 0)
+                    count = value;
                 else
-                    Console.WriteLine("Ooops! You can by only {0} product units", count);
+                    Console.WriteLine("Ooops! Can't be negative amount of products");
             }
         }
     }
+    // Shop with managers, users and products
     class Shop
     {
+        // Manaders array
         private Manager[] managers;
         public Manager this[int index]
         {
@@ -131,10 +177,49 @@ namespace Unit7
                 else
                     return null;
             }
+        }
+        public Manager this[string index]
+        {
+            get
+            {
+                foreach (var manager in managers)
+                {
+                    if (index == manager.Login)
+                    {
+                        return manager;
+                    }
+                }
+                Console.WriteLine("Ooops! Unknown manager you looking for");
+                return null;
+            }
+        }
+        // Array of users
+        private User[] users;
+        public User this[byte index]
+        {
+            get
+            {
+                if (index >= 0 && index < users.Length)
+                    return users[index];
+                else
+                    return null;
+            }
             set
             {
-                if (index >= 0 && index < managers.Length)
-                    managers[index] = value;
+                if (index >= 0 && index < users.Length)
+                    users[index] = value;
+            }
+        }
+        // Array of products
+        private Product[] products;
+        public Product this[short index]
+        {
+            get
+            {
+                if (index >= 0 && index < products.Length)
+                    return products[index];
+                else
+                    return null;
             }
         }
     }
@@ -158,10 +243,9 @@ namespace Unit7
     {
         static void Main(string[] args)
         {
-            Product<int> prod = new Product<int>();
-            prod.Count = 10;
+         
             Console.ReadLine();
-            
+
         }
     }
 }
